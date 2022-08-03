f112b31b847e687a48999e9f625a5801dbdfccbf
Evgenezak <zakharov.evgene@gmail.com>
2022-08-03 15:48:09 +0300

    feat: add funk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2 != 0) {
+    return false;
+  }
+
+  let halfLength = str.length/2;
+  let stackOfBrakets = [];
+  for (let i=0; i<str.length; i++) {
+    let lastSign = stackOfBrakets[stackOfBrakets.length-1];
+    let sample = [lastSign, str[i]];
+    if (bracketsCheck(sample, bracketsConfig)){
+      stackOfBrakets.pop();
+    } else {
+      stackOfBrakets.push(str[i]);
+    }
+  }
+  if (stackOfBrakets.length > 0){
+    return false
+  }
+  return true;
+}
+
+function bracketsCheck(sample, bracketsConfig){
+  for (let i=0; i< bracketsConfig.length; i++) {
+    if ( sample[0] === bracketsConfig[i][0] && sample[1] === bracketsConfig[i][1] ) {
+      return true;
+    }
+  }
+  return false;
 }
